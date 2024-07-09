3e7bedb95b89f399f91f0cdcd13c08b0cfa6d28b
vdang1120 <vdang1120@gmail.com>
2024-07-09 00:00:43 -0500

    updates

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from weather import get_current_weather
 from waitress import serve
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -22,13 +23,22 @@ def get_weather():
     #City not found by API
     if not weather_data['cod'] == 200:
         return render_template('city-not-found.html')
+    
+    print(weather_data)
 
     return render_template(
         "weather.html",
         title=weather_data["name"],
         status=weather_data["weather"][0]["description"].capitalize(),
         temp=f"{weather_data["main"]["temp"]:.1f}",
+        h=f"{weather_data["main"]["temp_max"]:.1f}",
+        l=f"{weather_data["main"]["temp_min"]:.1f}",
         feels_like=f"{weather_data["main"]["feels_like"]:.1f}",
+        icon = weather_data['weather'][0]['icon'],
+        wind=f"{weather_data["wind"]["speed"]:.1f}",
+        windkph = f"{weather_data["wind"]["speed"] *1.609 :.1f}",
+        precip = f"{weather_data['rain']['1h']:.2f}" if 'rain' in weather_data and '1h' in weather_data['rain'] else "0",
+        precipcm = f"{weather_data['rain']['1h'] * 25.4:.2f}" if 'rain' in weather_data and '1h' in weather_data['rain'] else "0",
     )
 
 if __name__ == "__main__":
